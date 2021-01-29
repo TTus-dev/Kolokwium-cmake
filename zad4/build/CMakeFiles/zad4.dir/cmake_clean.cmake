66c73f74245bbd3461b28453cbe3e08650931649
Tomasz Tobor <fampakarmelka@gmail.com>
2021-01-29 16:49:36 +0100

    Pliki kolokwium nr 2 (cmake)

---
+++ zad4/build/CMakeFiles/zad4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zad4.dir/src/libtest.cpp.o"
+  "zad4.pdb"
+  "zad4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zad4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
